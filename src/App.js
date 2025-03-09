e0c55e3959483f00a985b75664885e524fabe060
Mohak Nigam <mohaknigam2907@gmail.com>
2025-03-09 21:47:21 +0530

    change-4

--- src/App.js
+++ src/App.js
@@ -2,9 +2,18 @@ import { useState } from "react";
 
 export default function Board() {
     const [text, setText] = useState("Original Text");
+    const [place, setPlace] = useState("");
+    const [timeAtLocation, settimeAtLocation] = useState("");
+    const [targetTimeZone, settargetTimeZone] = useState("");
 
     const changeText = () => {
-        setText("Text changed!");
+        const result = `
+            Place: ${place}
+           timeAtLocation: ${timeAtLocation}
+            targetTimeZone: ${targetTimeZone}
+           ` ;
+
+        setText(result);
     };
 return(<>
 <section class='container'>
@@ -15,15 +24,27 @@ return(<>
         <input type='text' placeholder='Enter the place'/>
     </div>
     <div class='input-box'>
-        <label>time1</label>
+        <label>time At Location:</label>
         <input type='time' placeholder='Enter the time'/>
         <div class='input-box'>
-        <label>time2</label>
-        <input type='time' placeholder='Enter the time'/>
+        <label>target Time Zone:</label>
+        <select
+                        value={targetTimeZone}
+                        onChange={(e) => settargetTimeZone(e.target.value)}
+                    >
+                        <option value="">Select a timezone</option>
+                        <option value="UTC">UTC</option>
+                        <option value="EST">EST (Eastern Standard Time)</option>
+                        <option value="PST">PST (Pacific Standard Time)</option>
+                        <option value="CST">CST (Central Standard Time)</option>
+                        <option value="IST">IST (Indian Standard Time)</option>
+                        <option value="GMT">GMT (Greenwich Mean Time)</option>
+                        <option value="AEST">AEST (Australian Eastern Standard Time)</option>
+                    </select>
     </div>
     <div id="box">{text}</div>
 
-<button type="button" onClick={changeText}>Click Me</button>
+<button type="button" onClick={changeText}>Convert</button>
     </div>
 </form>
 </section>
